ad87d7d71cf89682e62b78ba955462c705f99479
sadulloradjabov <sadullojonradjabov@gmail.com>
2023-11-07 13:48:44 +0500

    js vazifa

---
+++ main.js
@@ -0,0 +1,10 @@
+let age = prompt("Yoshingiz nechida")
+
+age = 2023 - age
+
+console.log(`Siz ${age} yilda tug'ilgansz`)
+
+let miqdor = prompt("Oylik ish haqqingizni kiriting")
+
+miqdor = -0.15* - miqdor
+console.log(`Siz Davlatga ${miqdor} so'm miqdorida soliq berishingiz kerak`)
